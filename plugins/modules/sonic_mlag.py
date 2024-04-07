dfe1ac11bbe8328e527099d65915c186884bda18
chaithra <chaithramallikarjuna@aviznetworks.com>
2024-04-07 16:00:50 +0530

    AAB: corrected the import file paths for mlag files

--- plugins/modules/sonic_mlag.py
+++ plugins/modules/sonic_mlag.py
@@ -117,10 +117,10 @@ from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.u
     EntityCollection,
     to_lines,
 )
-from ansible_collections.aviznetworks.sonic_fmcli.plugins.module_utils.network.sonic.sonic import run_commands
-from ansible_collections.aviznetworks.sonic_fmcli.plugins.module_utils.network.sonic.utils.utils import command_list_str_to_dict
-from ansible_collections.aviznetworks.sonic_fmcli.plugins.module_utils.network.sonic.configs.mlag import MLAGConfig
-from ansible_collections.aviznetworks.sonic_fmcli.plugins.module_utils.network.sonic.argspecification.mlag_config import MlagArgs
+from ansible_collections.aviznetworks.ansible.plugins.module_utils.network.sonic.sonic import run_commands
+from ansible_collections.aviznetworks.ansible.plugins.module_utils.network.sonic.utils.utils import command_list_str_to_dict
+from ansible_collections.aviznetworks.ansible.plugins.module_utils.network.sonic.configs.mlag import MLAGConfig
+from ansible_collections.aviznetworks.ansible.plugins.module_utils.network.sonic.argspecification.mlag_config import MlagArgs
 
 
 
